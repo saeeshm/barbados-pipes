380a1b360e498c63a2f52d89e6785903840e0762
Saeesh Mangwani <saeeshm@gmail.com>
2020-05-04 14:56:08 -0400

    created basemaps for BWA districs and parishes with leaflet.
    Replaced spatial datasets with data projected in EPSG 4326 (latlong) in order to be consistent with the projection standards for leaflet and resolve the issue of data being displaced from base-tiles. The new datasets were generated using reprojection and then export from arcmap.

--- maps.R
+++ maps.R
@@ -8,11 +8,56 @@ library(tidyverse)
 library(sf)
 library(tmap)
 library(readr)
+library(leaflet)
 
-# Reading data
+# Reading data as sf objects
 pipes <- read_csv("data/final_db.csv")
-bwa_districts <- read_sf("data/Bwa_districts.shp")
-pipes_sdb <- read_sf("data/Final_Database_2020.shp")
-bb_parish <- read_sf("data/BB_parishes.shp")
+bwa_districts <- read_sf("data/Bwa_districts_latlong.shp")
+pipes_sdb <- read_sf("data/pipes_latlong.shp")
+bb_parish <- read_sf("data/BB_parishes_latlong.shp")
 
-# 
+# Calling the colorFactor function that can return a 'pal' function which generates a color palette based on an input length
+pal <- colorFactor(
+  palette = "viridis",
+  domain = bwa_districts$OBJECTID
+)
+
+# Creating a basemap using BWA districts as divisions
+district_base <- leaflet(bwa_districts) %>% 
+  addPolygons(color = ~pal(OBJECTID), 
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              highlightOptions = highlightOptions(color = "white", 
+                                                  weight = 2,
+                                                  bringToFront = TRUE),
+              popup = paste0("<b>Name: </b>",
+                             bwa_districts$DISTR_NAME,
+                             "<br>",
+                             "<b>Area: </b>",
+                             round(bwa_districts$Shape_Area, 2), " sqr m")
+  )
+# Creating a basemap using parishes as divisions
+parish_base <- leaflet(bb_parish) %>% 
+  addPolygons(color = ~pal(OBJECTID), 
+              weight = 1, 
+              smoothFactor = 0.5,
+              opacity = 1.0, 
+              fillOpacity = 0.5,
+              highlightOptions = highlightOptions(color = "white", 
+                                                  weight = 2,
+                                                  bringToFront = TRUE),
+              popup = paste0("<b>Name: </b>",
+                             bb_parish$NAME,
+                             "<br>",
+                             "<b>Population (In 1990): </b>",
+                             bb_parish$POP_1990,
+                             "<br>",
+                             "<b>Area: </b>",
+                             round(bb_parish$Shape_Area, 2), " sqr m")
+  )
+
+leaflet(pipes_sdb) %>% addPolylines()
+
+
