448e573ffe3dec38e94ccccbc089176b0ebb67bc
Saeesh Mangwani <saeeshm@gmail.com>
2020-05-05 16:40:00 -0400

    Scripts implementing 2 different packages for fuzzy logic models. First attempt at translating the fuzzy model from matlab to R

---
+++ fuzzyModel_fuzzyR.R
@@ -0,0 +1,112 @@
+# Name: Saeesh Mangwani
+# Date: 2020-05-04
+# Description: Implementing a fuzzy rules based system for classifying risk using the FuzzyR package.
+
+# ==== Loading libraries ====
+# A family of fundamental packages that assist with the manipulation, tidying and visualization of data in R
+library(tidyverse)
+library(FuzzyR)
+
+# Loading pipe data (both sf and dataframe)
+# pipes <- read_csv("data/final_db.csv")
+# pipes_sdb <- read_sf("data/pipes_latlong.shp")
+
+# Creating a new fuzzy inference system (fis) - most parameters are kept at default since the defualts are consistent with how Tara has defined her
+# fis
+fis <- newfis("hotspot_model2020")
+
+# Add input and output variables to this fis
+fis <- addvar(fis, 'input', 'diameter', c(0, 20))
+fis <- addvar(fis, 'input', 'land_use', c(0, 1))
+fis <- addvar(fis, 'input', 'month', c(1, 12))
+fis <- addvar(fis, 'input', 'pressure', c(-115, 270))
+fis <- addvar(fis, 'input', 'rainfall', c(0, 220))
+fis <- addvar(fis, 'output', 'risk', c(1, 4))
+
+# Adding membership functions (mfs) to each input variable (since the built-in function for adding mfs doesn't seem to be working, I wrote a custom
+# function for doing it. Not this is not a robust function, i.e it does not contain any error checking and is merely a workaround to the lack of
+# functionality in the package function)
+addmf_custom <- function(fis, var_type, index, name, mf_type, params){
+  # Getting a count of how many mfs are already present and using it to locate where to add the new mf
+  num_mf <- length(fis[[var_type]][[index]]$mf) + 1
+  # Creating an emtpy list at the location we want, that we'll then populate with values
+  fis[[var_type]][[index]]$mf[[num_mf]] <- list()
+  # Adding the name
+  fis[[var_type]][[index]]$mf[[num_mf]]$name <- name
+  # Adding the type
+  fis[[var_type]][[index]]$mf[[num_mf]]$type <- mf_type
+  # Adding the parameters
+  fis[[var_type]][[index]]$mf[[num_mf]]$params <- params
+  # returning the edited fis
+  fis
+}
+
+# membership functions for diameter
+fis <- addmf_custom(fis, 'input', 1, 'small', 'trapmf', c(-0.5, 3, 4, 5))
+fis <- addmf_custom(fis, 'input', 1, 'medium', 'trapmf', c(4, 5, 6, 7))
+fis <- addmf_custom(fis, 'input', 1, 'large', 'trapmf', c(6, 7, 20, 27))
+
+# membership functions for landuse
+fis <- addmf_custom(fis, 'input', 2, 'rural', 'trapmf', c(-0.36, -0.04, 0.1, 0.2))
+fis <- addmf_custom(fis, 'input', 2, 'urban', 'trapmf', c(0.8, 0.9, 1.04, 1.36))
+
+# membership functions for month
+fis <- addmf_custom(fis, 'input', 3, 'very_dry', 'trapmf', c(-2.96, 0.56, 4, 4.4))
+fis <- addmf_custom(fis, 'input', 3, 'dry', 'trapmf',  c(4.6, 5, 7, 7.4))
+fis <- addmf_custom(fis, 'input', 3, 'wet', 'trapmf', c(7.6, 8, 12.44, 15.96))
+
+# membership functions for pressure
+fis <- addmf_custom(fis, 'input', 4, 'normal', 'trapmf', c(-25, 15, 75, 80))
+fis <- addmf_custom(fis, 'input', 4, 'high', 'trapmf', c(75, 80, 285.4, 408.6))
+
+# membership functions for rainfall
+fis <- addmf_custom(fis, 'input', 5, 'very_dry', 'trapmf', c(-79.2, -8.8, 80, 85))
+fis <- addmf_custom(fis, 'input', 5, 'dry', 'trapmf',  c(80, 85, 105, 110))
+fis <- addmf_custom(fis, 'input', 5, 'wet', 'trapmf', c(195, 200, 228.8, 299.2))
+fis <- addmf_custom(fis, 'input', 5, 'average', 'trapmf', c(105, 110, 195, 200))
+
+# membership functions for risk
+fis <- addmf_custom(fis, 'output', 1, 'low', 'trimf', c(-1.6, 0, 1))
+fis <- addmf_custom(fis, 'output', 1, 'medium', 'trimf',  c(0.4, 1.6, 2.4))
+fis <- addmf_custom(fis, 'output', 1, 'peak', 'trimf', c(3, 4, 5.6))
+fis <- addmf_custom(fis, 'output', 1, 'high', 'trimf', c(1.6, 2.8, 4))
+
+
+# Adding fuzzy logic rules
+ruleList <- rbind(c(1,0,0,0,0,4,1,1), 
+                  c(2,0,0,0,0,2,1,1),
+                  c(3,0,0,0,0,1,1,1),
+                  c(0,1,0,0,0,1,1,1),
+                  c(0,2,0,0,0,2,1,1),
+                  c(1,0,1,0,0,3,1,1),
+                  c(1,0,2,0,0,4,1,1),
+                  c(2,0,-3,0,0,2,1,1),
+                  c(-3,0,3,0,0,2,1,1),
+                  c(1,0,0,2,0,3,1,1),
+                  c(-3,0,0,0,3,2,1,1),
+                  c(-3,0,0,0,2,4,1,1),
+                  c(-3,0,0,0,1,3,1,1))
+fis <- addrule(fis, ruleList)
+
+# Seeing the rules in linguistic terms for verification
+showrule(fis)
+
+# Getting a tabular summary of the fis model
+showfis(fis)
+
+# Some visualization tools to understand the fis and its components
+# A 3d graphical model of the fis and its functional parameters
+gensurf(fis)
+# A 2d graph of all membership functions in a variable
+plotmf(fis, 'input', 5)
+
+# Evaluating the fis model using provided inputs to generate a crisp value for risk
+
+# The inputs must be provided as a matrix, number of inputs (columns) by number of outputs (rows):
+input_stack <- matrix(c(2, 1, 3, 260, 20), 1, 5)
+evalfis(input_stack, fis)
+
+test_fis <- tipper()
+evalfis(matrix(c(1,2),1,2), test_fis)[1]
+
+tipperGUI2()
---
+++ fuzzyModel_sets.R
@@ -0,0 +1,100 @@
+# Name: Saeesh Mangwani
+# Date: 2020-05-04
+# Description: Implementing a fuzzy rules based system for classifying risk using the sets package.
+
+# ==== Loading libraries ====
+# A family of fundamental packages that assist with the manipulation, tidying and visualization of data in R
+library(tidyverse)
+library(sets)
+
+# Loading pipe data (both sf and dataframe)
+pipes <- read_csv("data/final_db.csv")
+pipes_sdb <- read_sf("data/pipes_latlong.shp")
+
+# Defining the range of the output, or the "universe" of values that the output set can take on
+# sets_options("universe", seq(from = 1, to = 4, by = 1))
+
+# Setting up the fuzzy variables:
+variables <- set(
+  diameter = 
+    fuzzy_variable(universe = seq(from = 0, to = 20, by = 0.1),
+                   small = 
+                     fuzzy_trapezoid(corners = c(-0.5, 3, 4, 5)),
+                   medium = 
+                     fuzzy_trapezoid(corners = c(4, 5, 6, 7)),
+                   large = 
+                     fuzzy_trapezoid(corners = c(6, 7, 20, 27))),
+  land_use = 
+    fuzzy_variable(universe = seq(from = 0, to = 1, by = 0.01),
+                   rural = 
+                     fuzzy_trapezoid(corners = c(-0.36, -0.04, 0.1, 0.2)),
+                   urban = 
+                     fuzzy_trapezoid(corners = c(0.8, 0.9, 1.04, 1.36))),
+  month = 
+    fuzzy_variable(universe = seq(from = 1, to = 12, by = 1),
+                   very_dry = 
+                     fuzzy_trapezoid(corners = c(-2.96, 0.56, 4, 4.4)),
+                   dry = 
+                     fuzzy_trapezoid(corners = c(4.6, 5, 7, 7.4)),
+                   wet = 
+                     fuzzy_trapezoid(corners = c(7.6, 8, 12.44, 15.96))),
+  pressure = 
+    fuzzy_variable(universe = seq(from = -115, to = 270, by = 0.5),
+                   normal = 
+                     fuzzy_trapezoid(corners = c(-25, 15, 75, 80)),
+                   high = 
+                     fuzzy_trapezoid(corners = c(75, 80, 285.4, 408.6))),
+  rainfall = fuzzy_variable(universe = seq(from = 0, to = 220, by = 0.5),
+                            very_dry = 
+                              fuzzy_trapezoid(corners = c(-79.2, -8.8, 80, 85)),
+                            dry = 
+                              fuzzy_trapezoid(corners = c(80, 85, 105, 110)),
+                            wet = 
+                              fuzzy_trapezoid(corners = c(195, 200, 228.8, 299.2)),
+                            average = 
+                              fuzzy_trapezoid(corners = c(105, 110, 195, 200))),
+  risk = fuzzy_variable(universe = seq(from = 0, to = 4, by = 1),
+                        low = 
+                          fuzzy_triangular(corners = c(-1.6, 0, 1)),
+                        medium = 
+                          fuzzy_triangular(corners = c(0.4, 1.6, 2.4)),
+                        peak = 
+                          fuzzy_triangular(corners = c(3, 4, 5.6)),
+                        high = 
+                          fuzzy_triangular(corners = c(1.6, 2.8, 4)))
+  )
+
+# Degining the rules that will be used for fuzzy matching the inputs to the output risk score
+rules <- set(
+  fuzzy_rule(diameter %is% small, risk %is% high),
+  fuzzy_rule(diameter %is% medium, risk %is% medium),
+  fuzzy_rule(diameter %is% large, risk %is% low),
+  fuzzy_rule(land_use %is% rural, risk %is% low),
+  fuzzy_rule(land_use %is% urban, risk %is% medium),
+  fuzzy_rule(diameter %is% small && month %is% very_dry, risk %is% peak),
+  fuzzy_rule(diameter %is% small && month %is% dry, risk %is% high),
+  fuzzy_rule(diameter %is% medium && !(month %is% wet), risk %is% medium),
+  fuzzy_rule(!(diameter %is% large) && month %is% wet, risk %is% medium),
+  fuzzy_rule(diameter %is% small && pressure %is% high, risk %is% peak),
+  fuzzy_rule(!(diameter %is% large) && rainfall %is% wet, risk %is% medium),
+  fuzzy_rule(!(diameter %is% large) && rainfall %is% dry, risk %is% high),
+  fuzzy_rule(!(diameter %is% large) && rainfall %is% very_dry, risk %is% peak)
+)
+
+# Combing the above into a system, and printing summaries and visualizations to understand the structure of this system
+system <- fuzzy_system(variables, rules)
+print(system)
+plot(system) ## plots variables
+
+# Testing the system through providing input to get inferences
+fi <- fuzzy_inference(system, vales = list(diameter = 3, land_use = 1, month = 8, pressure = 20, rainfall =  60), implication = "minimum")
+
+# plotting resulting fuzzy set
+plot(fi)
+
+## defuzzifying to get a risk score using the centroid method
+gset_defuzzify(fi, "centroid")
+
+## reset universe
+# sets_options("universe", NULL)
+# # }
